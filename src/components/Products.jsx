aa1554cdee2248b9bc35d99682e3710f4096146c
chiagoze pio <nduagubapio@gmail.com>
2024-08-14 11:44:06 +0100

    enabled filtering the products array

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import { useGetProductsQuery } from '../Utils/RTK/ProductApi'
-import { useDispatch, useSelector } from 'react-redux'
-import { setProducts } from '../Utils/features/Products/productsSlice'
-import { Button, Card, Spin } from 'antd'
-import { Link } from 'react-router-dom'
-import { CarOutlined, CarryOutFilled, CarTwoTone, ShopOutlined } from '@ant-design/icons'
+import React, { useRef, useState } from "react";
+import { useGetProductsQuery } from "../Utils/RTK/ProductApi";
+import { useDispatch, useSelector } from "react-redux";
+import { setProducts } from "../Utils/features/Products/productsSlice";
+import { Button, Card, Input, Spin } from "antd";
+import { Link } from "react-router-dom";
+import { ShopOutlined } from "@ant-design/icons";
 
 const Products = () => {
-    const {data : Product, isLoading} = useGetProductsQuery()
-    
-    const allProducts = Product?.products
-    const dispatch  = useDispatch()
+  const { data: Product, isLoading } = useGetProductsQuery();
+  const [search, setSearch] = useState("");
+  const allProducts = Product?.products;
+  const dispatch = useDispatch();
 
-    dispatch(setProducts(allProducts))
-    const products = useSelector(state =>  state.products.products)
+  dispatch(setProducts(allProducts));
+  const products = useSelector((state) => state.products.products);
 
-   // console.log(products);
-
-    /* availabilityStatus
+  /* availabilityStatus
 : 
 "Low Stock"
 brand
@@ -97,35 +95,87 @@ warrantyInformation
 weight
 : 
 2 */
+
+  return (
+    <div className="flex-grow p-5">
+      {isLoading && <Spin spinning={true} />}
+      {!isLoading && (
+        <div>
+          <div className="flex justify-center items-center w-full h-[2.7rem] mb-2 ">
+            <Input
+              className=" w-[250px] h-[2.2rem] "
+              placeholder="Search"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+          {search ? (
+            <SearchResults search={search} />
+          ) : (
+            <div className="flex flex-wrap gap-4">
+              {products.map((product) => (
+                <Card key={product.id} className="w-fit h-fit !p-2">
+                  <div>
+                    <img
+                      src={product.images[0]}
+                      className="w-full h-[30%] sm:h-[100px]"
+                    />
+                  </div>
+                  <h3 className="text-red-700">{product.title}</h3>
+                  <div className="flex justify-between flex-wrap sm:flex-nowrap">
+                    <p>{product?.brand}</p>
+                    <span>
+                      <span className="text-red-700">$</span>
+                      {product.price}
+                    </span>
+                  </div>
+                  <Button className="w-full h-[2rem]">
+                    Add to cart <ShopOutlined className="text-red-700" />
+                  </Button>
+                </Card>
+              ))}
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+const SearchResults = ({search}) => {
+    const products = useSelector((state) => state.products.products);
+    const searchProducts = products.filter(product => product.title.toLowerCase().includes(search.toLowerCase()))
+    
     
   return (
-    <div className='flex-grow p-5'>
-        {isLoading && <Spin spinning={true}/>}
-        {!isLoading &&
-                <div className='flex flex-wrap gap-4'>
-                {products.map(product => (
-                     <Card
-                      key={product.id}
-                      className='w-fit h-fit !p-2'
-        
-                     >
-                        <div>
-                            <img src={product.images[0]} className='w-full h-[30%] sm:h-[100px]'/>
-                        </div>
-                        <h3 className='text-red-700'>{product.title}</h3>
-                        <div className='flex justify-between flex-wrap sm:flex-nowrap'>
-                            <p>{product?.brand}</p>
-                            <span>{product.price}</span>
-                        </div>
-                        <Button className='w-full h-[2rem]'>Add to cart <ShopOutlined className='text-red-700'/></Button>
-                    </Card> 
-                ))}
+    
+    <>
+      <div>
+      <div className="flex flex-wrap gap-4">
+              {searchProducts.map((product) => (
+                <Card key={product.id} className="w-fit h-fit !p-2">
+                  <div>
+                    <img
+                      src={product.images[0]}
+                      className="w-full h-[30%] sm:h-[100px]"
+                    />
+                  </div>
+                  <h3 className="text-red-700">{product.title}</h3>
+                  <div className="flex justify-between flex-wrap sm:flex-nowrap">
+                    <p>{product?.brand}</p>
+                    <span>
+                      <span className="text-red-700">$</span>
+                      {product.price}
+                    </span>
+                  </div>
+                  <Button className="w-full h-[2rem]">
+                    Add to cart <ShopOutlined className="text-red-700" />
+                  </Button>
+                </Card>
+              ))}
             </div>
-        }
-        
-        
-    </div>
-  )
-}
-
-export default Products
+      </div>
+    </>
+  );
+};
+export default Products;
